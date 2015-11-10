c5b353b38430532d568b601632d7f98e0552d45e
mimoto.t <mimoto@w-tokyodo.com>
2015-11-10 11:52:03 +0900

    Pull Production Env Theme

--- mimosafa.dev/wp-content/themes/mmsf-child-theme/functions.php
+++ mimosafa.dev/wp-content/themes/mmsf-child-theme/functions.php
@@ -33,3 +33,20 @@ add_action( 'wp_enqueue_scripts', function() {
 		$theme->get( 'Version' )
 	);
 }, 11 );
+
+add_action( 'wp_footer', function() {
+	if ( ! is_user_logged_in() ) {
+		echo <<<EOF
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-26079619-2', 'auto');
+  ga('send', 'pageview');
+
+</script>
+EOF;
+	}
+} );
